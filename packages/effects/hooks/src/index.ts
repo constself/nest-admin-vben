236dc97afdc08c7f20a36fe396398cd76164d13a
constPc <constself@outlook.com>
2025-01-06 11:11:54 +0800

    feat

--- packages/effects/hooks/src/index.ts
+++ packages/effects/hooks/src/index.ts
@@ -2,6 +2,7 @@ export * from './use-app-config';
 export { useBoolean } from './use-boolean';
 export * from './use-content-maximize';
 export * from './use-design-tokens';
+export * from './use-hover-toggle';
 export { useLoading } from './use-loading';
 export * from './use-pagination';
 export * from './use-refresh';
